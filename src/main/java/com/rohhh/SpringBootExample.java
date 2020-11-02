b4070c1299796d3de594ffca0c2748c444405afb
Modugu Rohith Reddy <modugu.rohith.reddy@ericsson.com>
2020-11-02 14:18:26 +0530

    first commit

---
+++ src/main/java/com/rohhh/SpringBootExample.java
@@ -0,0 +1,12 @@
+package com.rohhh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExample {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExample.class, args);
+        System.out.println("Rohith");
+    }
+}
